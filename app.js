93081cb1bffd2a3c15a2f35db683ee90d4470a16
Vikram Chandramohan <vikramchandramohan@gmail.com>
2026-09-18 16:10:12 +0100

    Refactors

--- app.js
+++ app.js
@@ -1,28 +1,31 @@
 const express = require('express');
 const ejs = require('ejs');
 const http = require('http');
+const multer = require('multer');
 const socketIo = require('socket.io');
 
 const app = express();
 const server = http.createServer(app);
 const io = socketIo(server);
+const parseMultipartForm = multer().none();
 
 app.use(express.urlencoded({ extended: true }));
 app.set('view engine', 'ejs');
 
 let latestData = {};
 
+const receiveEncounter = (req, res) => {
+    latestData = req.body;
+    io.emit('dataUpdate', latestData);
+    res.json({ success: true });
+};
+
 //routing path
 app.get('/', (req, res) => {
     res.render('index', { appName: 'My App' });
 });
 
-app.post('/endpoint', (req, res) => {
-    latestData = req.body;
-    // Emit update to all connected clients
-    io.emit('dataUpdate', latestData);
-    res.json({ success: true });
-});
+app.post('/endpoint', parseMultipartForm, receiveEncounter);
 
 // WebSocket connection
 io.on('connection', (socket) => {
